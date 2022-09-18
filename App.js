6a53f3e5eabed2577a8205729dd0b333794b505b
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-09-18 17:53:58 +0300

    setting up aws amplify auth

--- App.js
+++ App.js
@@ -2,20 +2,22 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Navigator from "./src/navigation";
+import { Amplify } from "aws-amplify";
+import awsconfig from "./src/aws-exports";
+import { withAuthenticator } from "aws-amplify-react-native";
+
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <View style={styles.container}>
-        {/* <FeedScreen /> */}
+      <SafeAreaProvider style={styles.container}>
         <Navigator />
         <StatusBar style="auto" />
-      </View>
-    </SafeAreaProvider>
+      </SafeAreaProvider>
   );
 };
 
-export default App;
+export default withAuthenticator(App);
 
 const styles = StyleSheet.create({
   container: {
